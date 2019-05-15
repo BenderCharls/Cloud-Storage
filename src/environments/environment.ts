f9ac2a3db85943bacd14da200c05ed35c3b55f8c
BenderCharls <carloscuellarbrito.94@hotmail.com>
2019-05-15 12:23:41 -0500

    Proyecto completo

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDY203u1kqrubJU5OduBbAm1C1CNWAJIzQ",
+    authDomain: "fir-auth-6ec69.firebaseapp.com",
+    databaseURL: "https://fir-auth-6ec69.firebaseio.com",
+    projectId: "fir-auth-6ec69",
+    storageBucket: "fir-auth-6ec69.appspot.com",
+    messagingSenderId: "515876394304",
+    appId: "1:515876394304:web:6f57598119e3955a"
+  }
+};
